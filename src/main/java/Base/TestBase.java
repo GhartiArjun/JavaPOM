710904e3a2ef2aaeb099be51240a8d113e7c7963
Sumi977 <sumanarai110@gmail.com>
2025-04-12 18:32:32 -0400

    added testbase class

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -8,45 +8,56 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 
 public class TestBase {
-    static WebDriver driver;
-    static Properties prop;
-    public TestBase() {
+    public static void pauseExecutionForSeconds(int seconds) {
         try {
-            prop = new Properties();
-            FileInputStream ip = new FileInputStream("/Users/sumanarai/Documents/JavaPOM/src" +
-                    "/main/java/Setup/config.properties");
-            prop.load(ip);
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    private static Properties loadProperties() {
+        Properties property = new Properties();
+        //System.getProperty("user.dir");
+        String propertyFilePath = "/Users/sumanarai/Documents/JavaPOM/src/main/java" +
+                "/Setup/config.properties";
+        try (FileInputStream input = new FileInputStream(propertyFilePath)) {
+            property.load(input);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return property;
     }
-    public static void initialization(String browsername){
-        if(browsername.equalsIgnoreCase("chrome")){
+
+
+    public static String getProperties(String propertyName) {
+        return loadProperties().getProperty(propertyName).trim();
+    }
+
+    public static WebDriver getDriver(String browserName) {
+        WebDriver driver = null;
+        if (browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
-        } else if (browsername.equalsIgnoreCase("firefox")) {
+        } else if (browserName.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver();
-            driver= new FirefoxDriver();
-        }else if (browsername.equalsIgnoreCase("egde")) {
-            WebDriverManager.edgedriver();
-            driver= new EdgeDriver();
-        }else if (browsername.equalsIgnoreCase("safari")) {
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("safari")) {
             WebDriverManager.safaridriver();
-            driver= new SafariDriver();
-        }else{
-            System.out.println("spelling mistake");
+            driver = new SafariDriver();
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver();
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Invalid Browser Name");
         }
-
-
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        return driver;
     }
 }
-
